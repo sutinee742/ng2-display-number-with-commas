8d6232023f2853b5cdd2d1c93600a649b98821cb
sutinee742 <sutinee742@gmail.com>
2017-04-25 22:21:45 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Display number with commas';
+  numForDisplay: string = '';
+  numForInput: number ;
+
+  genNumWithComma() {
+    console.log('User input number: '+this.numForInput);
+    var numStr = this.numForInput+'';
+    var num = numStr.replace(/\d(?=(?:\d{3})+$)/g, '$&,');
+    console.log('Display number: '+num);
+    this.numForDisplay = num;
+  }
+
+
 }
